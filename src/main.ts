c494dd39306d0a51ee7ca9d6d698f17460e813a0
NasirSultan <rainasirsultan123@gmail.com>
2025-09-26 03:32:51 +0500

    doen

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 6000);
+
+
+
+ //  Enable CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:61561', // Flutter web debug server
+      'http://127.0.0.1:54206', // Sometimes Flutter uses 127.0.0.1
+    ],
+    credentials: true,
+  });
 }
 bootstrap();
